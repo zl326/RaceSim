3f949355743740a4ae8c829626526f74a361ecac
zl326 <tom_my_li@hotmail.com>
2019-08-12 19:55:10 +0100

    Cases and run file

---
+++ Model/Run.py
@@ -0,0 +1,12 @@
+
+
+class Simulation:
+    
+    def __init__(self, settings):
+        self.settings = settings
+        
+        
+        
+simulation = Simulation(1)
+
+print(simulation.settings)
